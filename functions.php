531448210572f1923e31b658fcb57a9e31a28397
oyeslama1624 <96531784+oyeslama1624@users.noreply.github.com>
2022-07-01 12:22:55 +0545

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,40 @@
+<!-- <?php
+// add_theme_support('menus');
+
+// register_nav_menus(array(
+//     'top-menu'=> __('Top Menu','theme')
+// )); -->
+
+
+// function add_class_li($Classes,$item,$args){
+//     if(isset($args -> li_class)){
+// $Classes[] = $sargs->li_class;
+//     }
+
+// if(isset($args->active_class) && in_arrray('current-menu-item',$classes)){
+//     $classes[] = $args->active_class;
+// }
+//     return $classes;
+// }
+// add_filter( 'nav_menu_css_class','add_class_li', 10, 3);
+
+// function add_anchor_class($attr,$item,$args){
+//     if(isset($args->a_class)){
+//         $attr['class']=$args->a_class;
+
+//     }
+//     return $attr;
+
+// }
+ 
+// add_filter ( 'nav_menu_link_attributes','add_anchor_class',10, 3);
+
+
+
+//  function is_blog()
+// {
+//     return (is_archive() || is_category() || is_home() || is_single() || is_search() || is_tag() )&&
+//     'post' == get_post_type();
+// }
+
+ 
